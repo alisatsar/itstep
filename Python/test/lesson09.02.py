75ff9616a9d6b918dfce10ffdf9b815d91594320
alisatsar <alisacookee@gmail.com>
2017-02-09 10:03:34 +0300

    variable

---
+++ Python/test/lesson09.02.py
@@ -0,0 +1,27 @@
+diff = 20
+print(diff)
+load = diff * 5
+print(load)
+diff = 5
+print(load)
+load = diff * 5
+print(load)
+a = 5
+b = 1.3
+print(a + b)
+print(type(a)) #показать тип переменной
+print(type(b))
+name = input('enter name')
+print(type(name))
+old = int(input("how old are U")) # запросить int Для переменной от пользователя
+print(type(old))
+old = float(input('old')) # запросить float Для переменной от пользователя (вводить через точку)
+print(type(old))
+print(2 ** 8)
+pow(2,3) #2 в степени 8
+abs(-9) #возвращает положительно значение (модуль)
+36%5
+type(float(input('old'))) #Дествие во внутренней выполняется первее
+print(float(5)) #выведет с точкой
+True #bool Значение (можно использовать как 1)
+type(True)
