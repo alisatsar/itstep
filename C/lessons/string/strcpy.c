6efef960ec298b4cc32aed2b1659dd0e02fc9858
Alisa Tsarova <alisacookee@gmail.com>
2017-03-22 21:51:13 +0300

    strcpy

---
+++ C/lessons/string/strcpy.c
@@ -0,0 +1,59 @@
+#define SIZE 40
+#define LIM 5
+
+char* s_gets(char* st, int n);
+
+int main()
+{
+	char list[LIM][SIZE];
+	char temp[SIZE];
+	int i = 0;
+	
+	printf("Enter %i words, that starter with letter q:\n", LIM);
+
+	while (i < LIM && s_gets(temp, SIZE))
+	{
+		if (temp[0] != 'q')
+		{
+			printf("%s not started with letter \"q\"", temp);
+		}
+		else
+		{
+			strcpy(list[i], temp);    //копирование из строки temp в строку list[i]
+			i++;
+		}
+	}
+	puts("The list of word, what has been taken:");
+	for (int i = 0; i < LIM; i++)
+	{
+		puts(list[i]);
+	}
+}
+
+char* s_gets(char* st, int n)
+{
+	char* retVal;
+	int i = 0;
+
+	retVal = fgets(st, n, stdin);
+
+	if (retVal)
+	{
+		while (st[i] != '\n' && st[i] != '\0')
+		{
+			i++;
+		}
+		if (st[i] == '\n')
+		{
+			st[i] = 0;
+		}
+		else
+		{
+			while (getchar() != '\n')
+			{
+				continue;
+			}
+		}
+	}
+	return retVal;
+}
