4a020b317987a53093115523f7a83eadb65dbea2
Alisa Tsarova <alisacookee@gmail.com>
2017-04-11 09:36:48 +0300

    create window

---
+++ Python/Tkinter/CreateWindow.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()     #класс создания окна
+
+root.title("First Window!") #заголовок окна
+root.geometry("500x500")    #размер окна
+
+root.mainloop()     #цикл который ожидает событие
+
+#pyw расшерение создает окно без консоли
