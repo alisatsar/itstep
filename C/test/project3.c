2c7baf0fb9373c34aa217d333969abd8a5fb33da
Alisa Tsarova <alisacookee@gmail.com>
2017-04-20 17:50:20 +0300

    Update project 3

--- C/test/project3.c
+++ C/test/project3.c
@@ -6,12 +6,12 @@ void checkPatternThree(int array[10], int* four, int* three, int* two, int* inde
 
 int main()
 {
-	int array[10] = { 1, 3, 1, 2, 3, 1, 2, 3, 1, 2 };
+	int array[10] = { 1, 3, 4, 1, 3, 4, 1, 3, 4, 2 };
 
 	int indexFour = 0;
 	int indexThree = 0;
 	int count = 1;
-	
+
 	for (int i = 9; i >= 0; i--)
 	{
 		for (int j = i - 1; j >= 0; j--)
@@ -65,35 +65,36 @@ void checkPattern(int array[10], int* four, int* three, int* indexThree, int* in
 		else if (*four != array[j - 1] && *three != array[j - 1] && array[j - 4] == array[j - 1])
 		{
 			int two = array[j - 1];
+			
 			*four = array[j + 1];
 			*three = array[j];
 			printf("\n\n%i\n", *four);
 			printf("%i\n", *three);
 			printf("%i\n", two);
-			int indexTwo = array[j - 1];
+			int indexTwo = j - 1;
 			*indexThree = j;
 			*indexFour = j + 1;
 			checkPatternThree(array, four, three, &two, &indexTwo, indexThree, indexFour, count);
 			break;
 		}
-	} 
+	}
 }
 
 void checkPatternThree(int array[10], int* four, int* three, int* two, int* indexTwo, int* indexThree, int* indexFour, int* count)
 {
-	
-	for (int k = *indexThree; k >= 0; k = k - 3)
+
+	for (int k = *indexTwo; k >= 0; k = k - 3)
 	{
 		if (*four == array[k - 1] && *three == array[k - 2] && *two == array[k - 3])
 		{
 			*indexFour = *indexFour;
-			*indexThree = k - 2;
+			*indexThree = k - 3;
 			*count = *count + 1;
 		}
 		else
 		{
 			break;
 		}
-		
+
 	}
 }
