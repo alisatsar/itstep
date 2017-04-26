6b4e0e9668c5d88eb568b50f466906b910f350f9
Alisa Tsarova <alisacookee@gmail.com>
2017-04-27 00:44:11 +0300

    1

---
+++ Python/Lessons/class/defineObject.py
@@ -0,0 +1,5 @@
+class FirstClass:                  # Определяет объект класса
+    def setdata(self, value):      # Определяет метод класса
+        self.data = value          # self – это экземпляр
+    def display(self):
+        print(self.data)           # self.data: данные экземпляров
