69ad0a8edfe90a7915632143995e8128208ce21e
Alisa Tsarova <alisacookee@gmail.com>
2017-04-27 12:25:46 +0300

    str.split()

--- Python/Lessons/string/str.split.py
+++ Python/Lessons/string/str.split.py
@@ -1,1 +1,7 @@
+a = "Ivan Aleksandrovich Petrov"
+print(a.split()[-1])    #[-1] означает вывести последнее слово
+#Petrov
+
+print(a.split("a" [-1]))    #[-1] означает максимальное количество разбиений
+#['Iv', 'n Aleks', 'ndrovich Petrov']
 
