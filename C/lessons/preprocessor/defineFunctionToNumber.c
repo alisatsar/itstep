972bbe333bbeb9fc1a1640c3afa9137c2ea9662d
Alisa Tsarova <alisacookee@gmail.com>
2017-05-15 10:26:44 +0300

    define function to number

---
+++ C/lessons/preprocessor/defineFunctionToNumber.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+void max1()
+{
+    printf("%s", __FUNCTION__);
+}
+ 
+void max2()
+{
+    printf(__FUNCTION__);
+}
+ 
+void max3()
+{
+    printf(__FUNCTION__);
+}
+ 
+void min1() { printf(__FUNCTION__); }
+void min2() { printf(__FUNCTION__); }
+void min3() { printf(__FUNCTION__); }
+ 
+#define VERSION 2
+ 
+#define EXEC2(foo, version) foo ## version();
+#define EXEC1(foo, version) EXEC2(foo, version)
+#define EXEC(foo) EXEC1(foo, VERSION)
+ 
+int main(int argCount, char* argValues[])
+{
+    EXEC(max);
+    EXEC(min);
+}
