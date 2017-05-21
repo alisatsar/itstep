cfbc32c238dea10ba619038792e974c8d40b04db
Alisa Tsarova <alisacookee@gmail.com>
2017-05-22 00:43:28 +0300

    data type

---
+++ CPP/Data Type/dataType.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Enter name!\n";  //character uotput
+	string firstName;         //definition
+	cin >> firstName;         //character input
+	cout << "Hello, " << firstName << "!\n";
+}
