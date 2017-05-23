29d618608e3a973202855ab805a5b793b97147a7
Alisa Tsarova <alisacookee@gmail.com>
2017-05-23 23:24:44 +0300

    Create main.cpp

---
+++ CPP/class/constructor/distance_constructor/main.cpp
@@ -0,0 +1,30 @@
+#include "distance.h"
+#include <iostream>
+
+int main()
+{
+	/*Distance* array = new Distance[5];
+	for (int i = 0; i < 5; i++)
+	{
+		array[i].Read();
+	}
+
+	Distance result;
+	result.SetData(0, 0);
+
+	for (int i = 0; i < 5; i++)
+	{
+		result.Add(array[i]);
+	}
+
+	result.Print();
+	delete[] array;
+
+	Distance d5(2, 5.5);
+	d5.Print();*/
+
+	Distance d6(2);
+	d6.Print();
+
+	return 0;
+}
