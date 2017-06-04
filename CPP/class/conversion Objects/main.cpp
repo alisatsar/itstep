4601ab1b8be32de9ee142f14657932256c57aec7
Alisa Tsarova <alisacookee@gmail.com>
2017-06-05 00:08:32 +0300

    Create main.cpp

---
+++ CPP/class/conversion Objects/main.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iosfwd>
+
+class Distance
+{
+private:
+	const float m_MTF;
+	int m_feet;
+	float m_inches;
+
+public:
+	Distance();
+	Distance(float meters); //конструктором преобразования
+	Distance(int feet, float inches);
+	void getDistance();
+	void showDistance() const;
+	operator float() const;
+	friend std::ostream& operator << (std::ostream& stream, Distance const& distance);
+};
+
+std::ostream& operator << (std::ostream& stream, Distance const& distance);
