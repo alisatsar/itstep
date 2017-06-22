bdf0c62359cf856cd374574ff4ebf1a11b5b769b
Alisa Tsarova <alisacookee@gmail.com>
2017-06-22 22:47:22 +0300

    Create main.cpp

---
+++ CPP/virtual function/Pointer/main.cpp
@@ -0,0 +1,43 @@
+#include "counten.h"
+#include <iostream>
+
+class Base
+{
+public:
+	void show()
+	{
+		std::cout << "Base\n";
+	}
+};
+
+class Child : public Base
+{
+public:
+	void show()
+	{
+		std::cout << "Child\n";
+	}
+};
+
+class Child2 : public Base
+{
+public:
+	void show()
+	{
+		std::cout << "Child2\n";
+	}
+};
+
+int main()
+{
+	Child child1;
+	Child child2;
+	Base* ptr;
+
+	ptr = &child1;
+	ptr->show();
+	ptr = &child2;
+	ptr->show();
+
+	return 0;
+}
