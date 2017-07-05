7aca7d8d5ea6de1474fcb33e053770c6b28e2137
Alisa Tsarova <alisacookee@gmail.com>
2017-07-05 15:41:27 +0300

    Create getline.cpp

---
+++ CPP/streams and files/getline.cpp
@@ -0,0 +1,24 @@
+#include <fstream> // Для файлового ввода/вывода
+#include <iostream>
+
+int main()
+{
+	std::ofstream file("Test.txt", std::ios::out | std::ios::trunc);
+
+	file << "Hello world!\n" << "How are you?\n" << "I\'m fine, thank you, and you?\n";
+
+	const int MAX = 80;
+	char buffer[MAX];
+
+	file.close(); //Закрыть файл
+
+	std::ifstream ifile("Test.txt");
+
+	while (!ifile.eof()) //цикл до EOF
+	{
+		ifile.getline(buffer, MAX);
+		std::cout << buffer << std::endl;
+	}
+
+	return 0;
+}
