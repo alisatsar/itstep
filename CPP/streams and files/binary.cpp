67b00dc4310078b801a23669127998083149e08b
Alisa Tsarova <alisacookee@gmail.com>
2017-07-06 09:48:00 +0300

    Update binary.cpp

--- CPP/streams and files/binary.cpp
+++ CPP/streams and files/binary.cpp
@@ -38,3 +38,37 @@ int main()
 	std::cout << "Correct data\n";
 	return 0;
 }
+
+/*#include <iostream>
+#include <fstream>
+#include <iomanip> //манипулатор ввода вывода
+
+int main()
+{
+	std::ofstream ofile("D:\\stream.bin", std::ios::out | std::ios::binary);
+	
+	int a = 1232323278;
+	double d = 2.1234093032;
+
+	ofile.write(reinterpret_cast<char*>(&a), sizeof(a));
+	ofile.write(reinterpret_cast<char*>(&d), sizeof(d));
+
+	ofile.close();
+
+	std::ifstream ifile("D:\\stream.bin", std::ios::in | std::ios::binary);
+
+	int b = 0;
+	double e = 0;
+
+	ifile.read(reinterpret_cast<char*>(&b), sizeof(b));
+	ifile.read(reinterpret_cast<char*>(&e), sizeof(e));
+
+	std::cout << std::hex << std::setiosflags(std::ios::showbase) <<  b << std::endl;
+	//std::hex манипулятор, который показывает в шестнадцатиричной системе 
+	//для вывода 0x std::setiosflags(std::ios::showbase)
+	std::cout << std::setprecision(12); //манипулятор, который принимает сколько
+										//будет выводить знаков после запятой (iomanip)
+	std::cout  << e << std::endl;
+
+	return 0;
+}*/
