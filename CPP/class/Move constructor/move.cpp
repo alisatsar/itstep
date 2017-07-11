0cc37cabb933434ab3f7af1f2c5f3f309e1381c2
Alisa Tsarova <alisacookee@gmail.com>
2017-07-11 20:55:36 +0300

    Update move.cpp

--- CPP/class/Move constructor/move.cpp
+++ CPP/class/Move constructor/move.cpp
@@ -1,4 +1,38 @@
-//h
+////////////////////////////////////////
+#include <utility>
+ X x;
+coll.insert(x); // Вставляет копию x (x можно использовать)
+
+coll.insert(x+x); // перемещает (или копирует) содержимое временного rvalue-значения
+
+coll.insert(std::move(x)); //перемещает (или копирует) содержимое x в объект coll
+
+
+namespace std
+{
+tepmlate<typename T, ...>
+class multiset
+{
+public:
+	insert(const T& x); //для lvalue-значений (копирует значение)
+	insert(T&& x); //для rvalue-значений (перемещает значение)
+};
+}
+
+class X
+{
+public:
+	X(const X& lvalue);//копирующий конструктор
+	X(X&& rvalue);//перемещающий конструктор
+};
+
+class X
+{
+public:
+	X& operator=(const X& lvalue); //оператор копирующего присваивания
+	X& operator=(X&& rvalue);//оператор перемещающего присваивания
+};
+///////////////////////////////////////////////////////////////////////
 
 Stack(Stack&& stack);
 Stack& operator=(Stack&& stack);
