ea48cd8e3f061d5a65d470989f154f8841a36b4c
Alisa Tsarova <alisacookee@gmail.com>
2017-07-13 21:59:48 +0300

    Update lambda.cpp

--- CPP/std/lambda.cpp
+++ CPP/std/lambda.cpp
@@ -1,6 +1,5 @@
 //////////////////////////////////////////////////
 #include <iostream>
-#include <vector>
 #include <string>
 
 int main()
@@ -74,8 +73,24 @@ int main()
 }
 
 //////////////////////////////////////////////////
+#include <iostream>
+#include <functional>
 
+std::function<int(int, int)> returnLambda()
+{
+	return [](int x, int y)
+	{
+		return x * y;
+	};
+};
 
+int main()
+{
+	auto lf = returnLambda();
+	std::cout << lf(6, 7) << std::endl;
+
+}
+///////////////////////////////////////////////////////////////
 
 #include <fstream>
 #include <vector>
