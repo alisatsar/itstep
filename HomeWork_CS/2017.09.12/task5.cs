801459cba16d755dc44d42aba3fcaafbbd20a73e
Alisa Tsarova <alisacookee@gmail.com>
2017-09-12 23:51:57 +0300

    Create task5.cs

---
+++ HomeWork_CS/2017.09.12/task5.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace _2017._09._12_HomeWork_task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberStr;
+            
+            int numberInt;
+
+            do
+            {
+                Console.WriteLine("Enter a number from 1 to 100000:");
+                numberStr = Console.ReadLine();
+
+                numberInt = int.Parse(numberStr);
+            } while (numberInt <= 0 || numberInt >= 100001);
+
+            int a = 10;
+            int b = 1;
+
+            //Console.Write((numberInt % 10) / b);
+
+            for (int i = 0; i < numberStr.Length; i++)
+            {
+                Console.Write((numberInt % a) / b);
+                a *= 10;
+                b *= 10;
+            }
+            Console.Write("\n");
+        }
+    }
+}
