007e2526287116be042c350c7a3e5c719aa58717
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:52:06 +0300

    Create Program.cs

---
+++ CS/2017.09.14_Class/2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace _2017._09._14_car
+{
+   class Program
+    {
+       static void Main(string[] args)
+       {
+          Tank tank = new Tank(3);
+          Driver driver = new Driver("Volodya", 5);
+          Car car = new Car("VW Polo", 100, driver, tank);
+       }
+    }
+}
