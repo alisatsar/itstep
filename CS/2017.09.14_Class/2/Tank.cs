b4271ae5703d16f63c3b76ca35e5246e2892d5b1
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:53:42 +0300

    Create Tank.cs

---
+++ CS/2017.09.14_Class/2/Tank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Tank
+    {
+        private int value;
+
+        public Tank(int value)
+        {
+            this.value = value;
+        }
+    }
+}
