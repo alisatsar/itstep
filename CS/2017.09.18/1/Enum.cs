7e2626f9ecfaad34c6907c274efabcdf0212863c
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:55:10 +0300

    Create Enum.cs

---
+++ CS/2017.09.18/1/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._18
+{
+    [Flags]
+    public enum Permission
+    {
+        Read = 1,
+        Write = 2,
+        Execute = 4,
+        Delete = 8,
+
+        ReadWrite = Read | Write
+    }
+}
