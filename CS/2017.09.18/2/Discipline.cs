9cf0e831cc31db1e5f99cfdeda4b6c705598fd51
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:58:05 +0300

    Create Discipline.cs

---
+++ CS/2017.09.18/2/Discipline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._18_2
+{
+    public enum Presence
+    {
+        Presence,
+        Late, 
+        Absent
+    }
+}
