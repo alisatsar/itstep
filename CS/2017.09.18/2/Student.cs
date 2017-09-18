a6484ea3c71898986ee55f09a722de6bac608cb7
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:58:25 +0300

    Create Student.cs

---
+++ CS/2017.09.18/2/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._18_2
+{
+    public class Student
+    {
+        private int mark;
+        private int[] marks = new int[100];
+        
+        public int Mark
+        {
+            get {return mark; }
+            set
+            {
+                mark = Math.Min(12, Math.Max(0, value));
+            }
+        }
+
+        public double AverageMark
+        {
+            get
+            {
+                return marks.Average();
+            }
+        }
+
+        public bool WanOnLesson
+        {
+            get
+            {
+                return Pres != Presence.Absent;
+            }
+        }
+
+        public Presence Pres { get; set; }
+        
+    }
+}
