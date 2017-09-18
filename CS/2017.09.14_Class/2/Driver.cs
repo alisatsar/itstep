ba842cec531e531c157a67f9ac6ca314df2f4b32
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:53:23 +0300

    Create Driver.cs

---
+++ CS/2017.09.14_Class/2/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Driver
+    {
+        private string name;
+        private int experience;
+
+        public Driver(string name, int experience)
+        {
+            this.name = name;
+            this.experience = experience;
+        }
+    }
+}
