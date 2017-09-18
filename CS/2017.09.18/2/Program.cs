10b4e1e1300bf2082fb01c7343c02206cc47407e
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:57:31 +0300

    Create Program.cs

---
+++ CS/2017.09.18/2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._18_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Student();
+            st.Mark = -100;
+
+            Console.WriteLine(st.Mark);
+        }
+    }
+}
