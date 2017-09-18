d9a6205ce7a38de75cf38580341b50d18e2c6103
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:55:53 +0300

    Create PointClass.cs

---
+++ CS/2017.09.18/1/PointClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._18
+{
+    public class PointClass
+    {
+        private int x;
+        private int y;
+
+        public PointClass(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Class x = {0}, y = {1}", x, y);
+        }
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+        public void SetY(int y)
+        {
+            this.y = y;
+        }
+
+    }
+}
