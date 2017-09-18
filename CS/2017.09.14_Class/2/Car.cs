c8bb6e5f7b6c4d4f375d1fa0cbe15078e21dee14
Alisa Tsarova <alisacookee@gmail.com>
2017-09-18 20:53:03 +0300

    Create Car.cs

---
+++ CS/2017.09.14_Class/2/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Car
+    {
+        private readonly string model;
+        private int speed;
+
+        private Driver driver;
+        private Tank tank;
+
+        public Car(string model, int speed, Driver driver, Tank tank)
+        {
+            this.model = model;
+            this.speed = speed;
+            this.tank = tank;
+            this.driver = driver;
+        }
+        public void Accolerate()
+        {
+            speed += 10;
+        }
+    }
+}
