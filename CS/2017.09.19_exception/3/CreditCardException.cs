91d1b3c58cd3aefaa5fc3ed345f7a960472ed85d
Alisa Tsarova <alisacookee@gmail.com>
2017-09-19 23:49:32 +0300

    Create CreditCardException.cs

---
+++ CS/2017.09.19_exception/3/CreditCardException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19_3
+{
+    [Serializable]
+    public class CreditCardException : Exception
+    {
+        public int RenaninigAteempts{get; set;}
+
+        public CreditCardException() { }
+        public CreditCardException(string message) : base(message) { }
+        public CreditCardException(string message, Exception inner) : base(message, inner) { }
+        protected CreditCardException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
