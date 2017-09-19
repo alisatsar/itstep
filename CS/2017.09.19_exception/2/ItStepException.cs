46eb4cc999ecb3a8c60a33de6ab706b8c4a30441
Alisa Tsarova <alisacookee@gmail.com>
2017-09-19 23:39:15 +0300

    Create ItStepException.cs

---
+++ CS/2017.09.19_exception/2/ItStepException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19_2
+{
+    [Serializable]
+    public class ItStepException : Exception
+    {
+        public ItStepException() { }
+        public ItStepException(string message) : base(message) { }
+        public ItStepException(string message, Exception inner) : base(message, inner) { }
+        protected ItStepException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context)
+                : base(info, context) { }
+        
+    }
+}
