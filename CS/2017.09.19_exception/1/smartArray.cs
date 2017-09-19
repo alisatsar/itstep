b3aa3aabcd5fbecda756702ea48974fbbcbd6af3
Alisa Tsarova <alisacookee@gmail.com>
2017-09-19 23:37:35 +0300

    Create smartArray.cs

---
+++ CS/2017.09.19_exception/1/smartArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19
+{
+    public class SmartArray
+    {
+        private int[] array;
+
+        public SmartArray(int count)
+        {
+            array = new int[count];
+
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i < 0)
+                {
+                    i = array.Length + i;
+                }
+                if (i > 0 && i < array.Length)
+                {
+                    return this[i];
+                }
+
+                throw new ArgumentOutOfRangeException();
+                
+            }
+        }
+    }
+}
