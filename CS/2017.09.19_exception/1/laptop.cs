c1e684d8710d77fd12a0fa6cac66b4e9957c6189
Alisa Tsarova <alisacookee@gmail.com>
2017-09-19 23:36:32 +0300

    Create laptop.cs

---
+++ CS/2017.09.19_exception/1/laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19
+{
+    public class Laptop
+    {
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+    }
+}
