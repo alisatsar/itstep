6874b97ef035c7513ed4378a820b803f21df4c2e
Alisa Tsarova <alisacookee@gmail.com>
2017-09-19 23:36:44 +0300

    Create program.cs

---
+++ CS/2017.09.19_exception/1/program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Shop();
+            shop.AddLaptop(new Laptop()
+            {
+                Brand = "Dell",
+                Price = 12.8m
+            });
+
+            if (shop[100] != null)
+            {
+                Console.WriteLine(shop[1].Brand);
+            }
+            
+            var laptopsBrandDell = shop["Dell"];
+
+            foreach(var laptop in shop["Dell"])
+            {
+                Console.WriteLine("{0}:{1}", laptop.Brand, laptop.Price);
+            }
+
+
+        }
+        
+    }
+}
