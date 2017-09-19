572e1fda41f2ef84c71a661ddf8a94cf1c18c462
Alisa Tsarova <alisacookee@gmail.com>
2017-09-19 23:48:12 +0300

    Create Account.cs

---
+++ CS/2017.09.19_exception/3/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;d
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19_3
+{
+    public class Account
+    {
+        private string Name;
+        private CreditCard Cards { get; set; }
+        private decimal Balans { get; set; }
+
+    }
+}
