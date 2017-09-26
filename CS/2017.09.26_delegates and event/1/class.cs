c4d3ea8f1d529dc61541bf94c0e0b2b04cd343d1
Alisa Tsarova <alisacookee@gmail.com>
2017-09-26 11:24:57 +0300

    Create class.cs

---
+++ CS/2017.09.26_delegates and event/1/class.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._26
+{
+    public delegate double MathOperation(double x, double y);
+
+}
