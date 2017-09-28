0f197f164a2e0858c8ac09674bb29299959e1336
Alisa Tsarova <alisacookee@gmail.com>
2017-09-28 19:01:52 +0300

    Create PointStruct.cs

---
+++ CS/2017.09.20_operator overloading/1/PointStruct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._20
+{
+    public struct PointStruct
+    {
+        public float xPos { get; set; }
+        public float yPos { get; set; }
+
+        public PointStruct(float x, float y)
+            :this()
+        {
+            xPos = x;
+            yPos = y;
+        }
+    }
+}
