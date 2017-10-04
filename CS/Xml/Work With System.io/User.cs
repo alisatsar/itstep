d5fae88e671da18ecc32833565b93789787ef2ca
Alisa Tsarova <alisacookee@gmail.com>
2017-10-05 00:12:13 +0300

    Create User.cs

---
+++ CS/Xml/Work With System.io/User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}
