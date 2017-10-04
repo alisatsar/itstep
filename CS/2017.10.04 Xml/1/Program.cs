d2185fa05eecd231f0146e4240a3234e2cd96a38
Alisa Tsarova <alisacookee@gmail.com>
2017-10-04 11:58:28 +0300

    Create Program.cs

---
+++ CS/2017.10.04 Xml/1/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._10._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student();
+            student.Answer("big");
+        }
+    }
+}
