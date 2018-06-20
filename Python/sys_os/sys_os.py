a84b762da5d455d9229b164a5f83b369ec73ee80
Alisa Tsarova <alisacookee@gmail.com>
2018-06-20 15:28:29 +0300

    Create sys_os.py

---
+++ Python/sys_os/sys_os.py
@@ -0,0 +1,12 @@
+>>> import sys
+>>> sys.platform #платформа
+'win32'
+о
+>>> sys.maxsize #число max int
+9223372036854775807
+>>> sys.version #версия Python
+'3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)]'
+>>> sys.path #пути модуля
+['', 'C:\\Users\\Tsarev\\AppData\\Local\\Programs\\Python\\Python36\\Lib\\idlelib', 'C:\\Users\\Tsarev\\AppData\\Local\\Programs\\Python\\Python36\\python36.zip', 'C:\\Users\\Tsarev\\AppData\\Local\\Programs\\Python\\Python36\\DLLs', 'C:\\Users\\Tsarev\\AppData\\Local\\Programs\\Python\\Python36\\lib', 'C:\\Users\\Tsarev\\AppData\\Local\\Programs\\Python\\Python36', 'C:\\Users\\Tsarev\\AppData\\Local\\Programs\\Python\\Python36\\lib\\site-packages']
+>>> sys.argv #аргументы командной строки
+['']
