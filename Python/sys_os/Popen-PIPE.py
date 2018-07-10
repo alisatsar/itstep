3e25b715fe2b4e8dcff5067e786cfb377eed02e1
Alisa Tsarova <alisacookee@gmail.com>
2018-07-10 21:01:52 +0300

    Update Popen-PIPE.py

--- Python/sys_os/Popen-PIPE.py
+++ Python/sys_os/Popen-PIPE.py
@@ -9,28 +9,30 @@ inp = input()
 open(‘hello-in.txt’, ‘w’).write(‘Hello ‘ + inp + ‘\n’)
 --------
 
->>> from subprocess import Popen, PIPE, call #импорт модуля
+>>> from subprocess import Popen, PIPE, call  #импорт модуля
 >>> x = call('python hello-out.py')
 >>> x
 2
->>> pipe = Popen('python hello-out.py', stdout=PIPE) #объект с открытым каналом sdtout
+>>> pipe = Popen('python hello-out.py', stdout=PIPE)  #объект с открытым каналом sdtout
 >>> pipe.communicate()[0] #return tuple stdout_data
 b'Hello shell world\r\n'
->>> pipe.returncode #код завершения
+>>> pipe.returncode   #код завершения
 0
 
 >>> pipe = Popen('python hello-out.py', stdout=PIPE)
 >>> pipe.stdout.read()
 b'Hello shell world\r\n'
->>> pipe.wait() #ожидает завершения дочернего процесса, устанавливает и возвращает код завершения
+>>> pipe.wait()   #ожидает завершения дочернего процесса, устанавливает и возвращает код завершения
 0
 
->>> pipe = Popen('python hello-in.py', stdin=PIPE)  #объект с открытым каналом sdtin
->>> pipe.stdin.write(b'Pokey\n') #записывает sdtin
+>>> pipe = Popen('python hello-in.py', stdin=PIPE)    #объект с открытым каналом sdtin
+>>> pipe.stdin.write(b'Pokey\n')  #записывает sdtin
 6
->>> pipe.stdin.close() #закрываем sdtin
+>>> pipe.stdin.close()  #закрываем sdtin
 >>> pipe.wait()
 0
 >>> open('input.txt').read()
 'Hello Pokey\n'
 >>> 
+
+
